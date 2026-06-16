d4fe2e08a4f23ef1a0fcbb6c6e3ce7ec4d24f307
iput2024-belle.tree.ibuki <belle.tree.japan@gmail.com>
2026-06-17 00:50:07 +0900

    不要ファイルの削除と軽微な修正

--- client/sensor/dht22.py
+++
@@ -1,9 +0,0 @@
-from . import dht22_takemoto as dht22
-
-
-def get_dht_data(gpio:int) -> tuple[float, float]:
-    if not gpio:
-        raise ValueError("DHT22 GPIO pin is not set. Please check config.py")
-    dht22_instance = dht22.DHT22(gpio)
-    tempe,hum,check = dht22_instance.read()
-    return float(tempe), float(hum)
